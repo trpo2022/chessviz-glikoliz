91eea4a1328627910826407f5d8e0bc309d1b0ad
Roman <glikoliz@users.noreply.github.com>
2022-03-11 20:17:12 +0700

    Implemented board output

---
+++ src/chessviz/print_board_stdout.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define board_size 8
+void print(char board[board_size][board_size])
+{
+    for (int i = 0; i < board_size; i++) 
+    {
+        printf("%d ", board_size - i);
+        for (int j = 0; j < board_size; j++) 
+        {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("  a b c d e f g j\n");
+}
