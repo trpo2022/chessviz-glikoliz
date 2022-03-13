0e45d7731559403422a644c6d45fa5188782d9dd
Roman <glikoliz@users.noreply.github.com>
2022-03-13 15:58:25 +0700

    Now the number of rows is defined here and now include move.h

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include "board_read.h"
 #include "print_board_stdout.h"
+#include <libchessviz/move.h>
+#include <stdio.h>
 #define board_size 8
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     char board[board_size][board_size]
             = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
@@ -13,7 +14,20 @@ int main(int argc, char **argv)
                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
+    int count = 1;
     print(*board);
-    board_read(argc, argv);
+    if (argc == 1) {
+        printf("Введите количество ходов: ");
+        scanf("%d", &count);
+    } else {
+        FILE* fp = fopen(argv[1], "r");
+        while (!feof(fp)) {
+            if (fgetc(fp) == '\n')
+                count++;
+        }
+        --count;
+        fclose(fp);
+    }
+    ok(board_read(argc, argv, count), count, *board);
     return 0;
 }
